75867e5be152896301c55bcbe442833b6350fd26
Connor Wilson <me@cwlsn.com>
2022-09-03 13:47:10 -0400

    Add Form submission and Beta key param (#22)

--- pages/sign-up.tsx
+++ pages/sign-up.tsx
@@ -53,6 +53,12 @@ export default function SignUp() {
     }
   }, [user]);
 
+  useEffect(() => {
+    if (typeof router.query.key === 'string') {
+      setBetaKey(router.query.key);
+    }
+  }, [router]);
+
   const reset = () => {
     setErrors([]);
     setEmail('');
@@ -160,96 +166,97 @@ export default function SignUp() {
           </List>
         </Alert>
       )}
+      <form onSubmit={handleSignUp}>
+        <Card withBorder shadow="md" mt="xl" sx={{ background: 'transparent' }}>
+          <Title order={4}>Beta Key</Title>
+          <Text color="dimmed">
+            Yards to Gain is currently in private beta, so you'll need a key to
+            sign up. If you don't know where to get one, you're probably not
+            invited.
+          </Text>
+          <Stack spacing={0} sx={{ width: '50%' }}>
+            <TextInput
+              mt="md"
+              size="lg"
+              label="Key"
+              icon={<Key />}
+              value={betaKey}
+              onChange={setBetaKey}
+              required
+            />
+          </Stack>
+        </Card>
 
-      <Card withBorder shadow="md" mt="xl" sx={{ background: 'transparent' }}>
-        <Title order={4}>Beta Key</Title>
-        <Text color="dimmed">
-          Yards to Gain is currently in private beta, so you'll need a key to
-          sign up. If you don't know where to get one, you're probably not
-          invited.
-        </Text>
-        <Stack spacing={0} sx={{ width: '50%' }}>
+        <Stack spacing="md" mt="xl" pb="xl" sx={{ width: '50%' }}>
+          <Title order={3}>Account Info</Title>
           <TextInput
-            mt="md"
-            size="lg"
-            label="Key"
-            icon={<Key />}
-            value={betaKey}
-            onChange={setBetaKey}
+            size="md"
+            label="Email"
+            description="We'll only spam you when we want"
+            icon={<Mail />}
+            value={email}
+            onChange={setEmail}
+            required
+          />
+          <PasswordInput
+            size="md"
+            label="Password"
+            description="Minimum 6 characters. Make sure to write this down somewhere."
+            icon={<Lock />}
+            value={password}
+            onChange={setPassword}
+            required
+          />
+          <PasswordInput
+            size="md"
+            label="Confirm Password"
+            description=""
+            icon={<Lock />}
+            value={passwordConfirm}
+            onChange={setPasswordConfirm}
             required
           />
         </Stack>
-      </Card>
-
-      <Stack spacing="md" mt="xl" pb="xl" sx={{ width: '50%' }}>
-        <Title order={3}>Account Info</Title>
-        <TextInput
-          size="md"
-          label="Email"
-          description="We'll only spam you when we want"
-          icon={<Mail />}
-          value={email}
-          onChange={setEmail}
-          required
-        />
-        <PasswordInput
-          size="md"
-          label="Password"
-          description="Minimum 6 characters. Make sure to write this down somewhere."
-          icon={<Lock />}
-          value={password}
-          onChange={setPassword}
-          required
-        />
-        <PasswordInput
-          size="md"
-          label="Confirm Password"
-          description=""
-          icon={<Lock />}
-          value={passwordConfirm}
-          onChange={setPasswordConfirm}
-          required
-        />
-      </Stack>
-      <Stack spacing="md" mt="xl" pb="xl" sx={{ width: '50%' }}>
-        <Title order={3}>Profile Info</Title>
-        <TextInput
-          size="md"
-          label="Team Name"
-          description="This will be your Fraud List team name"
-          icon={<BallAmericanFootball />}
-          value={teamName}
-          onChange={setTeamName}
-          required
-        />
-        <TextInput
-          size="md"
-          label="Name"
-          description="Your real name, don't be a dick"
-          icon={<Pencil />}
-          value={name}
-          onChange={setName}
-        />
-        <TextInput
-          size="md"
-          label="Nickname"
-          description="Your personal nickname, not a team name"
-          icon={<User />}
-          value={nickname}
-          onChange={setNickname}
-          required
-        />
-      </Stack>
-      <Card mt="xl" shadow="md" withBorder>
-        <Group spacing="sm">
-          <Button onClick={handleSignUp} loading={loading}>
-            Create Account
-          </Button>
-          <Button onClick={reset} variant="subtle">
-            Reset
-          </Button>
-        </Group>
-      </Card>
+        <Stack spacing="md" mt="xl" pb="xl" sx={{ width: '50%' }}>
+          <Title order={3}>Profile Info</Title>
+          <TextInput
+            size="md"
+            label="Team Name"
+            description="This will be your Fraud List team name"
+            icon={<BallAmericanFootball />}
+            value={teamName}
+            onChange={setTeamName}
+            required
+          />
+          <TextInput
+            size="md"
+            label="Name"
+            description="Your real name, don't be a dick"
+            icon={<Pencil />}
+            value={name}
+            onChange={setName}
+          />
+          <TextInput
+            size="md"
+            label="Nickname"
+            description="Your personal nickname, not a team name"
+            icon={<User />}
+            value={nickname}
+            onChange={setNickname}
+            required
+          />
+        </Stack>
+        <Card mt="xl" shadow="md" withBorder>
+          <Group spacing="sm">
+            <Button type="submit" onClick={handleSignUp} loading={loading}>
+              Create Account
+            </Button>
+            <Button onClick={reset} variant="subtle">
+              Reset
+            </Button>
+          </Group>
+        </Card>
+      </form>
     </Container>
   );
 }
