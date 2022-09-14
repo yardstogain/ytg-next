5944ce7afda919837c1971549b41ef899ef0aa0b
Connor Wilson <me@cwlsn.com>
2022-09-14 09:33:40 -0400

    fix semantic error

--- pages/fraud-list/ladder.tsx
+++ pages/fraud-list/ladder.tsx
@@ -13,8 +13,10 @@ import {
   withPageAuth,
 } from '@supabase/auth-helpers-nextjs';
 import { PageHeader } from 'components';
+import { schedule } from 'data/schedule2022';
 import {
   currencyFormatter,
+  getCurrentWeek,
   getLadderColor,
   getTeamIcon,
   renderPageTitle,
@@ -33,21 +35,25 @@ export const getServerSideProps = withPageAuth({
   redirectTo: '/login?f=ar',
   async getServerSideProps(ctx) {
     // const { user } = await getUser(ctx);
-    // const weekData = getCurrentWeek(schedule);
+    const currentWeekNumber = getCurrentWeek(schedule).week;
 
-    const { data } = await supabaseServerClient(ctx)
+    const { data: usersWithFraudData } = await supabaseServerClient(ctx)
       .from<UserWithFraudData>('profile')
       .select('*, fraudPicks(*), fraudListWinnings(*)');
 
-    return { props: { usersWithFraudData: data } };
+    return { props: { usersWithFraudData, currentWeekNumber } };
   },
 });
 
 type LadderPageProps = {
   usersWithFraudData: UserWithFraudData[];
+  currentWeekNumber: number;
 };
 
-export default function LadderPage({ usersWithFraudData }: LadderPageProps) {
+export default function LadderPage({
+  usersWithFraudData,
+  currentWeekNumber,
+}: LadderPageProps) {
   const sortedData = usersWithFraudData
     .map((user) => {
       const totalWinnings = user.fraudListWinnings.reduce((acc, curr) => {
@@ -71,7 +77,7 @@ export default function LadderPage({ usersWithFraudData }: LadderPageProps) {
         icon={<Ladder size={48} />}
         iconColor="indigo"
       />
-      <Table verticalSpacing="sm" mt="xl">
+      <Table verticalSpacing="xs" mt="xl">
         <thead>
           <tr>
             <th>Player</th>
@@ -80,104 +86,111 @@ export default function LadderPage({ usersWithFraudData }: LadderPageProps) {
             <th style={{ textAlign: 'right' }}>Total Winnings</th>
           </tr>
         </thead>
-        {sortedData.map((user, idx) => {
-          // TODO: wont scale, season
-          const mostPicked = user.fraudPicks.reduce<TeamSlug[]>((acc, curr) => {
-            if (curr.season === 2022) {
-              return [...acc, ...curr.picks];
-            }
-            return acc;
-          }, []);
+        <tbody>
+          {sortedData.map((user, idx) => {
+            // TODO: wont scale, season
+            const mostPicked = user.fraudPicks.reduce<TeamSlug[]>(
+              (acc, curr) => {
+                if (curr.season === 2022 && curr.week < currentWeekNumber) {
+                  return [...acc, ...curr.picks];
+                }
+                return acc;
+              },
+              [],
+            );
 
-          return (
-            <Box
-              component="tr"
-              sx={(theme) => ({
-                borderBottom:
-                  idx === 2
-                    ? `2px solid ${theme.colors.gray[7]}`
-                    : `1px solid ${theme.colors.gray[9]}`,
-              })}
-            >
-              <td>
-                <Group spacing={0}>
-                  <Box sx={{ width: idx < 3 ? 64 : 32 }}>
-                    <Text
-                      size={idx < 3 ? 40 : 24}
-                      weight="bold"
-                      color={getLadderColor(idx + 1)}
-                      align="right"
-                    >
-                      {idx === 0 && <Crown size={32} />}
-                      {idx < 3 && idx > 0 && <Trophy size={24} />}
-                      {idx + 1}
-                    </Text>
-                  </Box>
-                  <Stack spacing={0} ml="sm" my="sm">
-                    <Text size="lg" weight={700}>
-                      {user.teamName}
-                    </Text>
-                    <Text
-                      size="sm"
-                      color="dimmed"
-                      sx={{
-                        lineHeight: 1,
-                      }}
-                    >
-                      <Link href={`/player/${user.slug}`} passHref>
-                        <Anchor color="dimmed">{user.nickname}</Anchor>
-                      </Link>
-                    </Text>
-                  </Stack>
-                </Group>
-              </td>
-              <td>
-                <Group spacing={0}>
-                  {mostPicked.map((team) => (
-                    <Avatar
-                      key={team}
-                      size={48}
-                      src={getTeamIcon(team)}
-                      radius="xl"
-                      p={4}
-                      ml={-8}
-                      sx={(theme) => ({
-                        borderWidth: 2,
-                        borderColor: theme.colors.dark[4],
-                        borderStyle: 'solid',
-                        background: theme.colors.dark[7],
-                      })}
-                    />
-                  ))}
-                </Group>
-              </td>
-              <td align="right">
-                <Text
-                  size={idx < 3 ? 36 : 24}
-                  weight="bolder"
-                  color={
-                    user.totalWinnings / user.fraudPicks.length > 0
-                      ? 'teal'
-                      : 'red'
-                  }
-                >
-                  {currencyFormatter.format(
-                    user.totalWinnings / user.fraudPicks.length,
-                  )}
-                </Text>
-              </td>
-              <td align="right">
-                <Text
-                  size={idx < 3 ? 36 : 24}
-                  weight="bolder"
-                  color={user.totalWinnings > 0 ? 'teal' : 'red'}
-                >
-                  {currencyFormatter.format(user.totalWinnings)}
-                </Text>
-              </td>
-            </Box>
-          );
-        })}
+            return (
+              <Box
+                component="tr"
+                key={user.id}
+                py={0}
+                sx={(theme) => ({
+                  borderBottom:
+                    idx === 2
+                      ? `2px solid ${theme.colors.gray[7]}`
+                      : `1px solid ${theme.colors.gray[9]}`,
+                })}
+              >
+                <td>
+                  <Group spacing={0}>
+                    <Box sx={{ width: idx < 3 ? 64 : 32 }}>
+                      <Text
+                        size={idx < 3 ? 40 : 24}
+                        weight="bold"
+                        color={getLadderColor(idx + 1)}
+                        align="right"
+                      >
+                        {idx === 0 && <Crown size={32} />}
+                        {idx < 3 && idx > 0 && <Trophy size={24} />}
+                        {idx + 1}
+                      </Text>
+                    </Box>
+                    <Stack spacing={0} ml="sm" my="sm">
+                      <Text size="lg" weight={700}>
+                        {user.teamName}
+                      </Text>
+                      <Text
+                        size="sm"
+                        color="dimmed"
+                        sx={{
+                          lineHeight: 1,
+                        }}
+                      >
+                        <Link href={`/player/${user.slug}`} passHref>
+                          <Anchor color="dimmed">{user.nickname}</Anchor>
+                        </Link>
+                      </Text>
+                    </Stack>
+                  </Group>
+                </td>
+                <td>
+                  <Group spacing={0}>
+                    {mostPicked.map((team) => (
+                      <Avatar
+                        key={team}
+                        size={48}
+                        src={getTeamIcon(team)}
+                        radius="xl"
+                        p={4}
+                        ml={-8}
+                        sx={(theme) => ({
+                          borderWidth: 2,
+                          borderColor: theme.colors.dark[4],
+                          borderStyle: 'solid',
+                          background: theme.colors.dark[7],
+                        })}
+                      />
+                    ))}
+                  </Group>
+                </td>
+                <td align="right">
+                  <Text
+                    size={idx < 3 ? 36 : 24}
+                    weight="bolder"
+                    color={
+                      user.totalWinnings / user.fraudPicks.length > 0
+                        ? 'teal'
+                        : 'red'
+                    }
+                  >
+                    {currencyFormatter.format(
+                      user.totalWinnings / user.fraudPicks.length,
+                    )}
+                  </Text>
+                </td>
+                <td align="right">
+                  <Text
+                    size={idx < 3 ? 36 : 24}
+                    weight="bolder"
+                    color={user.totalWinnings > 0 ? 'teal' : 'red'}
+                  >
+                    {currencyFormatter.format(user.totalWinnings)}
+                  </Text>
+                </td>
+              </Box>
+            );
+          })}
+        </tbody>
       </Table>
       {sortedData.length === 0 && (
         <Text size="lg" mt="lg" color="dimmed" align="center">
